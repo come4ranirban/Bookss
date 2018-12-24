3a65442de0d3950166e1467c2492a92fb441c492
asarka1x <come4ranirban@gmail.com>
2018-12-24 19:48:32 +0530

    savadhyaya v1.0

--- app/src/main/java/com/nayakaurn/bookss/AboutUsPage.java
+++ app/src/main/java/com/nayakaurn/bookss/AboutUsPage.java
@@ -18,8 +18,8 @@ public class AboutUsPage extends Fragment {
         View v= inflater.inflate(R.layout.aboutus, container, false);
         TextView aboutus= (TextView)v.findViewById(R.id.aboutus);
         StaticVariableClass.toolbartxt.setText("About us");
-        String aboutustext= "Created by <b>Bhavika Technosolutions</b> for 2nd PUC Karnataka Exam. This app contains 2nd PUC Karnataka Exam Solved papers which will help to prepare for Exam in a smart and efficient way. Access Model Papers and Study Material With Solutions.<br><br>"+
-                "Bhavika  TechnoSolutions is a professional Website and App Designing and development Company located in Bangalore India. Bhavika TechnoSolutions dedicated to provide web based solutions to wide variety of clients. We provide customer-oriented web design services and more importantly, deliver them 100 % effectively.";
+        String aboutustext= "Created by <b>Bhavika Technosolutions</b> for 2nd PUC Karnataka Exam. This app contains <b>2nd PUC Karnataka Exam Solved papers</b> which will help to prepare for Exam in a smart and efficient way. Access Model Papers and Study Material With Solutions.<br><br>"+
+                "<b>Bhavika  TechnoSolutions</b> is a professional Website and App Designing and development Company located in Bangalore India. Bhavika TechnoSolutions dedicated to provide web based solutions to wide variety of clients. We provide customer-oriented web design services and more importantly, deliver them 100 % effectively.";
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             aboutus.setText(Html.fromHtml(aboutustext, Html.FROM_HTML_MODE_LEGACY)) ;
         } else {
